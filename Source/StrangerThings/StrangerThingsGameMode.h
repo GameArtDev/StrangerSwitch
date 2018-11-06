ac041737321ca55f8173a0319658afb1bb47a9f7
ema-barr <emanuele.barracchia@hotmail.it>
2018-11-06 21:29:12 +0100

    Initial commit

---
+++ Source/StrangerThings/StrangerThingsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StrangerThingsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AStrangerThingsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AStrangerThingsGameMode();
+};
+
+
+
